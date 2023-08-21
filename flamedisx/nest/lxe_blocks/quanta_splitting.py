67c53c3cff5eb2e8b2aac2e6e1ce8651e302858f
JoshuaGreenLZ <joshua.green@physics.ox.ac.uk>
2023-08-21 16:32:45 +0100

    remove kwargs

--- flamedisx/nest/lxe_blocks/quanta_splitting.py
+++ flamedisx/nest/lxe_blocks/quanta_splitting.py
@@ -93,7 +93,7 @@ class MakePhotonsElectronsNR(fd.Block):
                     normal_dist_nq = tfp.distributions.Normal(loc=nq_mean,
                                                               scale=tf.sqrt(nq_mean * fano) + 1e-10) 
                     p_nq_1D=normal_dist_nq.cdf(unique_quanta + 0.5) - normal_dist_nq.cdf(unique_quanta - 0.5)
-                p_nq=cutsom_grad_gather(params=p_nq_1D,indices=index_nq[:,o],batch_dims=0)
+                p_nq=cutsom_grad_gather(p_nq_1D,index_nq[:,o],0)
                 p_nq=tf.reshape(p_nq,[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2]])#restore event dimension
                 p_nq=tf.repeat(p_nq[:,:,:,o],tf.shape(nq)[3],axis=3)
 
@@ -105,7 +105,7 @@ class MakePhotonsElectronsNR(fd.Block):
                 nq_2D=tf.repeat(unique_quanta[:,o],tf.shape(_ions_produced_1D)[0],axis=1)
                 ni_2D=tf.repeat(_ions_produced_1D[o,:],tf.shape(unique_quanta)[0],axis=0)
                 p_ni_2D=tfp.distributions.Binomial(total_count=nq_2D, probs=alpha).prob(ni_2D)
-                p_ni=cutsom_grad_gather(params=p_ni_2D,indices=index_nq[:,o],batch_dims=0)
+                p_ni=cutsom_grad_gather(p_ni_2D,index_nq[:,o],0)
                 p_ni=tf.reshape(tf.reshape(p_ni,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2],tf.shape(nq)[3]])
 
             else:
@@ -143,7 +143,7 @@ class MakePhotonsElectronsNR(fd.Block):
                 p_ni = tf.repeat(p_ni_1D[o,:], tf.shape(ions_produced)[2], axis=0)
                 p_ni = tf.repeat(p_ni[o,:, :], tf.shape(ions_produced)[1], axis=0)
                 p_ni = tf.repeat(p_ni[o,:, :, :], tf.shape(ions_produced)[0], axis=0)
-                p_nq=cutsom_grad_gather(params=p_nq_2D,indices=index_nq[:,o],batch_dims=0)
+                p_nq=cutsom_grad_gather(p_nq_2D,index_nq[:,o],0)
                 p_nq=tf.reshape(tf.reshape(p_nq,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2],tf.shape(nq)[3]])
 
 
@@ -180,7 +180,7 @@ class MakePhotonsElectronsNR(fd.Block):
                                                                         owens_t_terms=owens_t_terms).prob(nel_2D)
 
             
-            p_nel=cutsom_grad_gather(params=p_nel_1D,indices=index_nel[:,o],batch_dims=0)
+            p_nel=cutsom_grad_gather(p_nel_1D,index_nel[:,o],0)
             p_nel=tf.reshape(tf.reshape(p_nel,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[3]])
             p_nel=tf.repeat(p_nel[:,:,o,:],tf.shape(nq)[2],axis=2)
 
