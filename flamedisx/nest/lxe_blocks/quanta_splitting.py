e270f9c8291acc2517d8c8431023ea9ad2f53475
JoshuaGreenLZ <joshua.green@physics.ox.ac.uk>
2023-08-25 12:23:23 +0100

    reverting performance improvement attempts

--- flamedisx/nest/lxe_blocks/quanta_splitting.py
+++ flamedisx/nest/lxe_blocks/quanta_splitting.py
@@ -80,7 +80,9 @@ class MakePhotonsElectronsNR(fd.Block):
                     normal_dist_nq = tfp.distributions.Normal(loc=nq_mean,
                                                               scale=tf.sqrt(nq_mean * fano) + 1e-10) 
                     p_nq_1D=normal_dist_nq.cdf(unique_quanta + 0.5) - normal_dist_nq.cdf(unique_quanta - 0.5)
-                p_nq=tf.gather(p_nq_1D,index_nq_4D)
+                p_nq=tf.gather_nd(params=p_nq_1D,indices=index_nq[:,o],batch_dims=0)
+                p_nq=tf.reshape(p_nq,[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2]])#restore event dimension
+                p_nq=tf.repeat(p_nq[:,:,:,o],tf.shape(nq)[3],axis=3)
 
                 ex_ratio = self.gimme('exciton_ratio', data_tensor=data_tensor, ptensor=ptensor,
                                       bonus_arg=energy)
@@ -90,7 +92,8 @@ class MakePhotonsElectronsNR(fd.Block):
                 nq_2D=tf.repeat(unique_quanta[:,o],tf.shape(_ions_produced_1D)[0],axis=1)
                 ni_2D=tf.repeat(_ions_produced_1D[o,:],tf.shape(unique_quanta)[0],axis=0)
                 p_ni_2D=tfp.distributions.Binomial(total_count=nq_2D, probs=alpha).prob(ni_2D)
-                p_ni=tf.gather(p_ni_2D,index_nq_3D)
+                p_ni=tf.gather_nd(params=p_ni_2D,indices=index_nq[:,o],batch_dims=0)
+                p_ni=tf.reshape(tf.reshape(p_ni,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2],tf.shape(nq)[3]])
 
             else:
                 yields = self.gimme('mean_yields', data_tensor=data_tensor, ptensor=ptensor,
@@ -127,8 +130,8 @@ class MakePhotonsElectronsNR(fd.Block):
                 p_ni = tf.repeat(p_ni_1D[o,:], tf.shape(ions_produced)[2], axis=0)
                 p_ni = tf.repeat(p_ni[o,:, :], tf.shape(ions_produced)[1], axis=0)
                 p_ni = tf.repeat(p_ni[o,:, :, :], tf.shape(ions_produced)[0], axis=0)
-                p_nq=tf.gather(p_nq_2D,index_nq_3D)
-               
+                p_nq=tf.gather_nd(params=p_nq_2D,indices=index_nq[:,o],batch_dims=0)
+                p_nq=tf.reshape(tf.reshape(p_nq,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2],tf.shape(nq)[3]])
 
 
             nel_2D=tf.repeat(unique_nel[:,o],tf.shape(_ions_produced_1D)[0],axis=1)
@@ -164,7 +167,9 @@ class MakePhotonsElectronsNR(fd.Block):
                                                                         owens_t_terms=owens_t_terms).prob(nel_2D)
 
             
-            p_nel=tf.gather(p_nel_1D,index_nel_3D)
+            p_nel=tf.gather_nd(params=p_nel_1D,indices=index_nel[:,o],batch_dims=0)
+            p_nel=tf.reshape(tf.reshape(p_nel,[-1]),[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[3]])
+            p_nel=tf.repeat(p_nel[:,:,o,:],tf.shape(nq)[2],axis=2)
 
             p_mult = p_nq * p_ni * p_nel
 
@@ -193,10 +198,7 @@ class MakePhotonsElectronsNR(fd.Block):
         # unique_quanta,index_nq=unique(nq[:,:,:,0])#nevts x nph x nel->unique_nq
         # unique_nel,index_nel=unique(electrons_produced[:,:,0,0])#nevts x nel->unique_nel
         unique_quanta,index_nq=tf.unique(tf.reshape(nq[:,:,:,0],[-1]))
-        index_nq_3D=tf.reshape(index_nq,[tf.shape(nq)[0],tf.shape(nq)[1],tf.shape(nq)[2]])
-        index_nq_4D=tf.repeat(index_nq_3D[:,:,:,o],tf.shape(nq)[3],axis=3)
         unique_nel,index_nel=tf.unique(tf.reshape(electrons_produced[:,:,0,0],[-1]))
-        index_nel_3D=tf.repeat(tf.reshape(index_nel,[tf.shape(nq)[0],tf.shape(nq)[1]])[:,:,o],tf.shape(nq)[2],axis=2)
 
         ions_min_initial = self.source._fetch('ions_produced_min', data_tensor=data_tensor)[:, 0, o]
         ions_min_initial = tf.repeat(ions_min_initial, tf.shape(ions_produced)[1], axis=1)
